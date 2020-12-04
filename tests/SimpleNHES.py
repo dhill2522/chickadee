d9521309c28b3b294b1e5318d7f47143d7413c35
Prism <andrewtrainor20@gmail.com>
2020-12-04 14:28:25 -0700

    added first simpleNHES without storage

--- tests/SimpleNHES.py
+++ tests/SimpleNHES.py
@@ -9,7 +9,11 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 n = 110 # number of time points
-time_horizon = np.linspace(0, 10 , n)
+
+time_horizon = np.linspace(0, n-1 , n)
+db_load = '../../data/Ercot/Ercot Load/ERCOT_load_2019.csv'
+Loaddata = pd.read_csv(db_load)
+Load = .5*Loaddata['ERCOT'].values[0:n]
 
 steam = chickadee.Resource('steam')
 electricity = chickadee.Resource('electricity')
@@ -23,14 +27,14 @@ def smr_transfer(data, meta):
     return data, meta
 
 
-smr_capacity = np.ones(n)*1200
-smr_ramp = 600*np.ones(n)
-smr_guess = 100*np.sin(time_horizon) + 300
+smr_capacity = np.ones(n)*1280*35
+smr_ramp = np.ones(n)*.03*1280*35
+smr_guess = np.ones(n)*.8*1280*35
 smr = chickadee.PyOptSparseComponent('smr', smr_capacity, smr_ramp, smr_ramp, steam,
                                 smr_transfer, smr_cost, produces=steam, guess=smr_guess)
 
 def turbine_transfer(data, meta):
-    effciency = 0.7  # Just a random guess at a turbine efficiency
+    effciency = 0.8  # Just a random guess at a turbine efficiency
 
     if steam in data:
         # Determine the electricity output for a given steam input
@@ -47,9 +51,9 @@ def turbine_transfer(data, meta):
 def turbine_cost(dispatch):
     return sum(-1 * dispatch[steam])
 
-turbine_capacity = np.ones(n)*1000
-turbine_guess = 100*np.sin(time_horizon) + 500
-turbine_ramp = 20*np.ones(n)
+turbine_capacity = np.ones(n)*1280*35
+turbine_guess = np.ones(n)*.8*1280*35
+turbine_ramp = np.ones(n)*.5*1280*35
 turbine = chickadee.PyOptSparseComponent('turbine', turbine_capacity,
                                 turbine_ramp, turbine_ramp, electricity,
                                 turbine_transfer, turbine_cost,
@@ -63,7 +67,7 @@ def elm_cost(dispatch):
     return sum(5.0 * dispatch[electricity])
 
 # elm_capacity = np.ones(n)*-800
-elm_capacity = -(100*np.sin(time_horizon) + 500)
+elm_capacity = -Load
 elm_ramp = 1e10*np.ones(n)
 elm = chickadee.PyOptSparseComponent('el_market', elm_capacity, elm_ramp, elm_ramp,
                                 electricity, el_market_transfer, elm_cost,
