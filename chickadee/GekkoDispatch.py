8d89198391a07ea0955ea20498b962d4295f37f8
Prism <andrewtrainor20@gmail.com>
2021-03-26 12:46:58 -0600

    I contributed!

--- chickadee/GekkoDispatch.py
+++ chickadee/GekkoDispatch.py
@@ -14,43 +14,6 @@ from itertools import chain
 from pprint import pformat
 from gekko import GEKKO
 
-class DispatchState(object):
-    '''Modeled after idaholab/HERON NumpyState object'''
-    def __init__(self, components: List[PyOptSparseComponent], time: List[float]):
-        s = {
-        }
-
-        for c in components:
-            s[c.name] = {}
-            for resource in c.get_resources():
-                s[c.name][resource] = np.zeros(len(time))
-
-        self.state = s
-        self.time = time
-
-    def set_activity(self, component: PyOptSparseComponent, resource, activity, i=None):
-        if i is None:
-            self.state[component.name][resource] = activity
-        else:
-            self.state[component.name][resource][i] = activity
-
-    def get_activity(self, component: PyOptSparseComponent, resource, i=None):
-        try:
-            if i is None:
-                return self.state[component.name][resource]
-            else:
-                return self.state[component.name][resource][i]
-        except Exception as err:
-            print(i)
-            raise err
-
-    def set_activity_vector(self, component: PyOptSparseComponent,
-                            resource, start, end, activity):
-        self.state[component.name][resource][start:end] = activity
-
-    def __repr__(self):
-        return pformat(self.state)
-
 
 class Gekko(Dispatcher):
     def __init__()
--- chickadee/MPC.py
+++ chickadee/MPC.py
@@ -3,11 +3,17 @@
 
 class ModelPredictiveController(object):
 
-    def __init__(self, components, dispatcher, pred_len=10):
+    def __init__(self, components, dispatcher, time_horizon, measurement_funcs, 
+                 component_name, cv_resource, set_point, pred_len=10):
         self.time_horizon = time_horizon
         self.components = components
         self.dispatcher = dispatcher
+        self.measure_funcs = measurement_funcs
+        self.component_name = component_name
+        self.cv_resource = cv_resource
         self.pred_len = pred_len
+        self.set_point = set_point
+
 
     def _opt_pred(self):
         '''Optimize the model response '''
