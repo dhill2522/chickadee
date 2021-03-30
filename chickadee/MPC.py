471763c517f3c2cd64deb89d2f6e8feae6dee56e
Daniel Hill <dhill2522@gmail.com>
2021-03-30 12:15:45 -0600

    structure rework

--- chickadee/MPC.py
+++ chickadee/MPC.py
@@ -1,28 +1,18 @@
 ''' Implementation of MPC using Chickadee models'''
-from PyOptSparseDispatch import PyOptSparse
+from PyOptSparseDispatch import PyOptSparse, DispatchState
 
 
 class MPController(object):
 
-    def __init__(self, components, dispatcher, time_horizon, measurement_funcs, 
-                 component_name, cv_resource, set_point, pred_len=10):
-        self.time_horizon = time_horizon
+    def __init__(self, components, dispatcher, measurement_funcs, 
+                 component_name, cv_resource, pred_len=10):
         self.components = components
         self.dispatcher = dispatcher
         self.measure_funcs = measurement_funcs
         self.component_name = component_name
         self.cv_resource = cv_resource
         self.pred_len = pred_len
-        self.set_point = set_point
 
-
-    def _opt_pred(self):
-        '''Optimize the model response '''
-	def objective(self):
-		MPC_comp = [x for x.name == component_name in dispatch.components]
-		return np.sum((MPC_comp[0][cv_resource] - set_point)**2)
-	sol = PyOptSparse.dispatch(self.components, self.time_horizon, self.objective)
-        return sol
 
     def control(self, time_horizon, set_point):
         '''Control a realtime system using the given model
@@ -32,5 +22,23 @@ class MPController(object):
             - optimize the system response as predicted by the model
             - apply the first set of control moves as suggested by the optimal system input
         '''
+        # Make data arrays
 
-        pass
+        # Make obj function
+        def objective(dispatch: DispatchState) -> float:
+            cv_disp = dispatch.state[self.component_name][self.cv_resource]
+    		return np.sum((cv_disp - set_point)**2)
+
+        for i, t in enumerate(time_horizon, set_point):
+            # Read in current state from measurement functions
+
+            # Update components with current state
+            for c in self.components:
+                c.ramp_rate = np.delete(c.ramp_rate, 0)
+                # Repeat for all arrays that need to be shortened
+
+            # Run the optimization
+            sol = PyOptSparse.dispatch(self.components, self.time_horizon, objective)
+            # Apply the control step from the optimized dispatch
+
+            # Wait the 
