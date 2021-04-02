09ba28827ef675e07885dda118f67f829952984c
Prism <andrewtrainor20@gmail.com>
2021-04-02 12:33:04 -0600

    updated MPC.py

--- chickadee/MPC.py
+++ chickadee/MPC.py
@@ -2,13 +2,14 @@
 from PyOptSparseDispatch import PyOptSparse, DispatchState
 
 
-class MPController(object):
+class MPC(object):
 
     def __init__(self, components, dispatcher, measurement_funcs, 
-                 component_name, cv_resource, pred_len=10):
+		 control_funcs, component_name, cv_resource, pred_len=10):
         self.components = components
         self.dispatcher = dispatcher
         self.measure_funcs = measurement_funcs
+	self.control_funcs = control_funcs
         self.component_name = component_name
         self.cv_resource = cv_resource
         self.pred_len = pred_len
@@ -22,23 +23,34 @@ class MPController(object):
             - optimize the system response as predicted by the model
             - apply the first set of control moves as suggested by the optimal system input
         '''
+	
+	import time
+
         # Make data arrays
 
         # Make obj function
         def objective(dispatch: DispatchState) -> float:
             cv_disp = dispatch.state[self.component_name][self.cv_resource]
-    		return np.sum((cv_disp - set_point)**2)
+    	    return np.sum((cv_disp - set_point)**2)
 
         for i, t in enumerate(time_horizon, set_point):
             # Read in current state from measurement functions
-
+	    start_time = time.time()
             # Update components with current state
-            for c in self.components:
+            for j, c in enumerate(self.components):
                 c.ramp_rate = np.delete(c.ramp_rate, 0)
                 # Repeat for all arrays that need to be shortened
+		c.capacity.append(self.measure_funcs[j]) # This assumes measure funcs were added in same order as components
 
             # Run the optimization
             sol = PyOptSparse.dispatch(self.components, self.time_horizon, objective)
-            # Apply the control step from the optimized dispatch
 
-            # Wait the 
+            # Apply the control step from the optimized dispatch
+	    for f in self.control_funcs:
+	        f(sol) 
+
+            # Wait the dt time in time history (assumes uniform dt) 
+	    end_time = time.time()
+	    dt = end_time - start_time
+	    dt_time_horizon = time_horizon[0] - time_horizon[1]
+	    time.sleep(dt_time_horizon - dt)
