d264b2053e07c638a9cbff951beb64812ec6860a
Prism <andrewtrainor20@gmail.com>
2021-04-12 17:10:10 -0600

    updated mpc

--- chickadee/MPC.py
+++ chickadee/MPC.py
@@ -5,15 +5,15 @@ import numpy as np
 
 class MPC(object):
 
-    def __init__(self, components, dispatcher, measurement_funcs, 
+    def __init__(self, components, measurement_funcs, 
 		 control_funcs, component_name, cv_resource, pred_len=10):
         self.components = components
-        self.dispatcher = dispatcher
         self.measure_funcs = measurement_funcs
         self.control_funcs = control_funcs
         self.component_name = component_name
         self.cv_resource = cv_resource
         self.pred_len = pred_len
+        self.solution = []
 
 
     def control(self, time_horizon, set_point):
@@ -31,29 +31,34 @@ class MPC(object):
             cv_disp = dispatch.state[self.component_name][self.cv_resource]
             return np.sum((cv_disp - set_point)**2)
 
-        for _ in time_horizon:
-            # Read in current state from measurement functions
+        # Iterate over time horizon
+        # FIXME: This currently stops 1 prediction length before end of time horizon
+        for i in range(len(time_horizon) - self.pred_len):      
             start_time = time.time()
+            ie = i + self.pred_len  # This is the end index for prediction window
 
             # Run the optimization
             dispatcher = PyOptSparse(window_length=self.pred_len)
-            sol = dispatcher.dispatch(self.components, time_horizon, objective)
+            sol = dispatcher.dispatch(self.components, time_horizon[i:ie], objective)
    
-                # Apply the control step from the optimized dispatch
+            # Apply the control step from the optimized dispatch
             for f in self.control_funcs:
                 f(sol)    
 
             # Update components with current state
             for c in self.components:
-                c.capacity = np.delete(c.capacity, 0)
-                # Repeat for all arrays that need to be shortened
-            np.append(c.capacity, self.measure_funcs[0])
+                if len(c.capacity) != self.pred_len:
+                    raise BaseException('haha got ya daniel!') 
+                    raise BaseException('Inputs for constraints need to have same length as pred_length')
+            self.solution.append(sol)
 
             # Wait the dt time in time history (assumes uniform dt) 
+            # dt changes based on the time steps in the time horizon
             end_time = time.time()
             dt = end_time - start_time
-            dt_time_horizon = time_horizon[0] - time_horizon[1]
+            dt_time_horizon = time_horizon[1] - time_horizon[0]
             sleep_time = dt_time_horizon - dt
             if sleep_time < 0:
-               raise("Warning you have tried to bend the rules of time you need to make your time steps bigger or shorten prediction window")
+               raise BaseException("Warning you have tried to bend the rules of time you need to make your time steps bigger or shorten prediction window")
             time.sleep(dt_time_horizon - dt)
+        return self.solution
